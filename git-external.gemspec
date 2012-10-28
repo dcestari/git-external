81f934e910acef0a21755231115d16dae55b6e77
Daniel Cestari <dcestari@gmail.com>
2012-10-28 12:31:27 -0430

    Refactor to keep all code in a library file
    Since having all the code in the binary file
    is harder to test and maintain. The code is
    better of on a library file that can be used
    by many binaries (in the future) or even an
    external program

--- git-external.gemspec
+++ git-external.gemspec
@@ -1,13 +1,15 @@
 Gem::Specification.new do |s|
-  s.name = 'git-external'
-  s.version = '0.1.1'
-  s.platform = Gem::Platform::RUBY
-  s.authors = ["Daniel Cestari"]
-  s.email = ["dcestari@gmail.com"]
-  s.homepage = "http://github.com/dcestari/git-external"
-  s.summary = "Git version of an svn:external, an alternative to Git Submodule"
+  s.name        = 'git-external'
+  s.version     = '0.2.0'
+  s.date        = '2012-10-28'
+  s.platform    = Gem::Platform::RUBY
+  s.authors     = ["Daniel Cestari"]
+  s.email       = ["dcestari@gmail.com"]
+  s.homepage    = "http://github.com/dcestari/git-external"
+  s.summary     = "Git version of an svn:external, an alternative to Git Submodule"
   s.description = "Extension for git which adds a command providing similar functionality to git submodules but without attaching each module to a single version"
 
   s.executables = ['git-external']
+  s.files       = ['lib/git_external.rb']
 end
 
---
+++ lib/git_external.rb
@@ -0,0 +1,107 @@
+class GitExternal
+  $root_dir       = `git rev-parse --show-toplevel`.chomp
+  $externals_file = "#{$root_dir}/.gitexternals"
+  $ignore_file    = "#{$root_dir}/.gitignore"
+  $configurations = {}
+
+  def usage
+    puts "Usage: git external add <repository-url> <path> [<branch>]"
+    puts "   or: git external init [--] [<path>...]"
+    puts "   or: git external update [--] [<path>...]"
+    puts "   or: git external cmd '<command>'"
+  end
+
+  def load_configuration
+    if File.file? $externals_file
+      lines = `git config -l -f #{$externals_file}`.split(/\n/)
+      $configurations = {}
+      lines.each do |line|
+        if line =~ /^external\.([^\.]+)\.([^=]+)=(.*)$/
+          $configurations[$1.chomp] ||= {}
+          $configurations[$1.chomp][$2.chomp] = $3.chomp
+        end
+      end
+    end
+  end
+
+  def print_configuration
+    $configurations.each do |name, config|
+      puts name
+      config.each do |key, value|
+        puts "\t#{key}: #{value}"
+      end
+    end
+  end
+
+  def normalize_url(url)
+    if url =~ /^\./
+      origin_url = `git config --get remote.origin.url`.chomp
+
+      unless origin_url =~ /^\w+:\/\//
+        if origin_url =~ /^([^:\/]+):([^:]+)/
+          origin_url = "ssh://#{$1}/#{$2}"
+        end
+      end
+
+      require 'uri'
+      uri = URI.parse URI.encode origin_url
+      uri.path = File.expand_path(url, uri.path)
+      uri.to_s
+    else
+      url
+    end
+  end
+
+  def init_external(url, path, branch='master')
+    require 'ftools'
+    unless File.directory? "#{path}/.git"
+      File.makedirs File.dirname(path)
+      url = normalize_url url
+      system "git clone #{url} #{path}"
+      system "cd #{path}; git checkout --track -b #{branch} origin/#{branch}"
+    end
+  end
+
+  def update_external(url, path, branch='master')
+    require 'ftools'
+    if File.directory? "#{path}/.git"
+     `cd #{path}; git pull origin #{branch}`
+    end
+  end
+
+  def command_add(url, path, branch='master')
+    `git config -f #{$externals_file} --add external.#{path}.path #{path}`
+    `git config -f #{$externals_file} --add external.#{path}.url #{url}`
+    `git config -f #{$externals_file} --add external.#{path}.branch #{branch}`
+    `echo "#{path}" >> #{$ignore_file}`
+  end
+
+  def command_rm(path)
+    `git config -f #{$externals_file} --unset external.#{path}.path`
+    `git config -f #{$externals_file} --unset external.#{path}.url`
+    `git config -f #{$externals_file} --unset external.#{path}.branch`
+    `git config -f #{$externals_file} --remove-section external.#{path}`
+    `perl -pi -e 's/\\Q#{path.gsub(/\//, '\/')}\\E\n//g' #{$ignore_file}`
+    File.delete $externals_file if `wc -l #{$externals_file}`.chomp.to_i == 0
+  end
+
+  def command_init
+    $configurations.each do |name, config|
+      puts name
+      init_external config["url"], config["path"], config["branch"]
+    end
+  end
+
+  def command_update
+    $configurations.each do |name, config|
+      update_external config["url"], config["path"], config["branch"]
+    end
+  end
+
+  def command_cmd(cmd)
+    $configurations.each do |name, config|
+      path = config['path']
+      system("echo #{path}; cd #{path}; #{cmd}")
+    end
+  end
+end
